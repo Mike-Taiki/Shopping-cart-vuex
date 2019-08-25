fc7595577c4272484a3571cab33f2390ec2ce45c
Michael Gomes <michael.21.2012@hotmail.com>
2019-08-25 20:45:32 -0300

    adicionado Vuex

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: { // = data  
+        products: []
+    },
+
+    getters: { // = computed properties
+        productsCount() {
+            // ...
+        }
+    },
+
+    actions: {
+        fetchProducts() {
+            // make the call
+            // run setProducts mutation
+        }
+    },
+
+    mutations: {
+        setProducts(state, products) {
+            // update products
+            state.products = products
+        }
+    }
+})
